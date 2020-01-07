5fc6ffcc31244f1a424d18855085ecb92cec4e13
Ben Nesbit <benesbit@yahoo.com>
2020-01-06 20:41:55 -0800

    Remove unused commented out code

--- main.py
+++ main.py
@@ -41,26 +41,3 @@ while not CRASHED:
 
 pygame.quit()
 quit()
-
-# # Define a main function
-# def main():
-#     # Create a surface on screen that has the size of 240 x 180
-#     screen = pygame.display.set_mode((240, 180))
-
-#     # Define a variable to control the main loop
-#     running = True
-
-#     # Main loop
-#     while running:
-#         # Event handling, gets all events from event queue
-#         for event in pygame.event.get():
-#             # Only do something if event is type QUIT
-#             if event.type == pygame.QUIT:
-#                 # Change running value to False, exiting main loop
-#                 running = False
-
-# # Run the main function only if this module is executed as the main script
-# # (if you import this as a module then nothing is executed)
-# if __name__ =="__main__":
-#     # Call the main function
-#     main()
