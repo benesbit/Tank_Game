edabd8d7f8448b7d61d94f5e797344c2966741c6
Ben Nesbit <benesbit@yahoo.com>
2020-01-07 00:48:22 -0800

    Transform scaling on image to shrink it down
    This is not perfect, but it works for now

--- main.py
+++ main.py
@@ -9,6 +9,9 @@ pygame.init()
 DISPLAY_WIDTH = 1050
 DISPLAY_HEIGHT = 620
 
+TANK_WIDTH = 45
+TANK_HEIGHT = 50
+
 GAME_DISPLAY = pygame.display.set_mode((DISPLAY_WIDTH, DISPLAY_HEIGHT))
 pygame.display.set_caption('Tank Game')
 
@@ -19,10 +22,12 @@ CLOCK = pygame.time.Clock()
 
 CRASHED = False
 
-CAR_IMG = pygame.image.load('images/tank_player_one.png')
+TANK_IMAGE_PLAYER_ONE = \
+    pygame.transform.scale(pygame.image.load('images/tank_player_one.png'), \
+        (TANK_WIDTH, TANK_HEIGHT))
 
 def car(x_location, y_location):
-    GAME_DISPLAY.blit(CAR_IMG, (x_location, y_location))
+    GAME_DISPLAY.blit(TANK_IMAGE_PLAYER_ONE, (x_location, y_location))
 
 X = (DISPLAY_WIDTH * 0.45)
 Y = (DISPLAY_HEIGHT * 0.8)
