200594414bda8c777fd4e79490cd8e3220782cef
Ben Nesbit <benesbit@yahoo.com>
2020-01-06 20:18:43 -0800

    Trying to get base code to work.
    Previous code seemed over-complicated, going off
    a different tutorial now.

--- main.py
+++ main.py
@@ -3,33 +3,35 @@
 # Import pygame module
 import pygame
 
-# Define a main function
-def main():
+# Initialize pygame module
+pygame.init()
 
-    # Initialize pygame module
-    pygame.init()
-    # Load and set the logo
-    logo = pygame.image.load("logo32x32.png")
-    pygame.display.set_icon(logo)
-    pygame.display.set_caption("minimal program")
+GAME_DISPLAY = pygame.display.set_mode((800, 600))
+pygame.display.set_caption('Tank Game')
 
-    # Create a surface on screen that has the size of 240 x 180
-    screen = pygame.display.set_mode((240, 180))
+CLOCK = pygame.time.Clock()
 
-    # Define a variable to control the main loop
-    running = True
 
-    # Main loop
-    while running:
-        # Event handling, gets all events from event queue
-        for event in pygame.event.get():
-            # Only do something if event is type QUIT
-            if event.type == pygame.QUIT:
-                # Change running value to False, exiting main loop
-                running = False
 
-# Run the main function only if this module is executed as the main script
-# (if you import this as a module then nothing is executed)
-if __name__ =="__main__":
-    # Call the main function
-    main()
+# # Define a main function
+# def main():
+#     # Create a surface on screen that has the size of 240 x 180
+#     screen = pygame.display.set_mode((240, 180))
+
+#     # Define a variable to control the main loop
+#     running = True
+
+#     # Main loop
+#     while running:
+#         # Event handling, gets all events from event queue
+#         for event in pygame.event.get():
+#             # Only do something if event is type QUIT
+#             if event.type == pygame.QUIT:
+#                 # Change running value to False, exiting main loop
+#                 running = False
+
+# # Run the main function only if this module is executed as the main script
+# # (if you import this as a module then nothing is executed)
+# if __name__ =="__main__":
+#     # Call the main function
+#     main()
